a02387109512aecf4adba4fdb85a9d0c2e4fdab1
iiscorpion <for.programing2002@gmail.com>
2025-12-30 15:00:49 +0100

    light and dark theme

--- app/page.tsx
+++ app/page.tsx
@@ -4,9 +4,6 @@ function Home() {
       <h1 className="h1-bold font-inter">
         The beginning to master next js seriously
       </h1>
-      <h1 className="h1-bold font-serif">
-        The beginning to master next js seriously
-      </h1>
     </div>
   );
 }
---
+++ components/navigation/navbar/index.tsx
@@ -0,0 +1,27 @@
+import Image from "next/image";
+import Link from "next/link";
+import Theme from "./Theme";
+
+function Navbar() {
+  return (
+    <nav className="flex-between background-light900_dark200 z-50 w-full p-6 gap-5 shadow-light-300 dark:shadow-none sm:px-12">
+      <Link href="/" className="flex items-center gap-1">
+        <Image
+          src="/images/site-logo.svg"
+          alt="Dev flow"
+          width={23}
+          height={23}
+        />
+        <p className="h2-bold font-space-grotesk text-dark-100 dark:text-light-900 max-sm:hidden">
+          Dev <span className="text-primary-500">flow</span>
+        </p>
+      </Link>
+      <p>Global Search</p>
+      <div className="flex-between gap-5">
+        <Theme />
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
